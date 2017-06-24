72fbecc2bb45fa8a58be42a67620cb7ef79c9c38
Phill Ayers <payers1@users.noreply.github.com>
2017-06-24 19:05:49 -0400

    Update pre-processor.js

--- pre-processor.js
+++ pre-processor.js
@@ -2,11 +2,8 @@
 const sass = require('node-sass')
 
 module.exports = (data, filename) => {
-  console.log('here', filename, data);
   return sass.renderSync({
     data,
     file: filename
-    // indentedSyntax: true,
-    // outputStyle: 'compressed'
   }).css.toString('utf8')
 }
