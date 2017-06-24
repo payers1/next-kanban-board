6e51126b8f5d73fdc899f86c4e8df004852fb428
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-06-24 19:33:17 -0400

    layout can be stateless component

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,21 @@
-import {Component} from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import { Container, Menu } from 'semantic-ui-react'
-import Router from 'next/router'
+import { Menu } from 'semantic-ui-react'
 
-export default class extends Component {
+export default ({children}) => (
+  <div style={{background: 'rgb(242, 242, 245)'}}>
+    <Head>
+      <title>NextJS KanBan</title>
+      <link rel='stylesheet' href='/static/css/bundle.css' />
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
+    </Head>
 
- render() {
-   return (<div style={{background: 'rgb(242, 242, 245)'}}>
-       <Head>
-         <title>NextJS KanBan</title>
-         <link rel='stylesheet' href='/static/css/bundle.css' />
-         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
-       </Head>
+    <Menu>
+      <Menu.Item name='home'>
+        <Link href='/'>  HOME </Link>
+      </Menu.Item>
+    </Menu>
 
-       <Menu>
-         <Menu.Item name='home' > <Link href='/'> HOME </Link> </Menu.Item>
-       </Menu>
-
-       <Container fluid>
-         {this.props.children}
-       </Container></div>)
- }
-}
+    {children}
+  </div>
+)
